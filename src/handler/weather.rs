ee57be80df29144c5293095d6c1f18e179460489
katayama <idnlnroocwetn22@gmail.com>
2025-11-08 23:30:13 +0900

    GooglemapのWeather API実行を追加

--- src/handler/weather.rs
+++ src/handler/weather.rs
@@ -1,5 +1,31 @@
-use axum::{Json, http::StatusCode};
+use axum::extract::Query;
+use dotenv::dotenv;
+use serde::Deserialize;
+use std::env;
 
-pub async fn weekly_weather() -> String {
-    "weekly_weather".to_string()
+#[derive(Debug, Deserialize)]
+pub struct WeeklyWeatherQuery {
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+pub async fn weekly_weather(location: Query<WeeklyWeatherQuery>) -> String {
+    let latitude = location.latitude;
+    let longitude = location.longitude;
+
+    let url = format!(
+        "https://weather.googleapis.com/v1/forecast/days:lookup?key={}location.latitude={}&location.longitude={}&days=7",
+        get_api_key(),
+        latitude,
+        longitude,
+    );
+
+    let response = reqwest::get(url).await.unwrap();
+    let body = response.text().await.unwrap();
+    body
+}
+
+fn get_api_key() -> String {
+    dotenv().ok();
+    env::var("GOOGLE_MAPS_API_KEY").expect("GOOGLE_MAPS_API_KEY must be set")
 }
